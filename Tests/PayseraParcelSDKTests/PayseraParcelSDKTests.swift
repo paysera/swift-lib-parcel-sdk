c17883a9563d212c5a67f8229490ad52fa1d46c4
Gerald Adorza <gerald.adorza@paysera.net>
2021-04-15 15:31:58 +0800

    Removed place holders

--- Tests/PayseraParcelSDKTests/PayseraParcelSDKTests.swift
+++ Tests/PayseraParcelSDKTests/PayseraParcelSDKTests.swift
@@ -7,7 +7,7 @@ import PromiseKit
 final class PayseraParcelSDKTests: XCTestCase {
     
     private lazy var parcelAPIClient: ParcelAPIClient = {
-        let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJwYXJjZWxfbmV0d29ya19hcGkiLCJpc3MiOiJhdXRoX2FwaSIsImV4cCI6MTYxODQ0MDE2MiwianRpIjoieENzYnk5RVc0aHpibFYydGpkd0ZoV0xRVG1UQTlqUkwiLCJwc3I6cyI6WyJsb2dnZWRfaW4iXSwicHNyOnUiOiI5NjQ4OTk0IiwicHNyOnNpZCI6IlRpZ3U5U1g5Tjc5LTNNTEhwWTlYbDZpZGhkUjI4eW4tIiwicHNyOmEiOnsidXNlcl9pZCI6Ijk2NDg5OTQifSwiaWF0IjoxNjE4Mzk2OTYyfQ.KwsC9i9C6764wqmzcQqQzs6aN4dZx5YQhrOm7PdDqL9TTREIZBRxS2bHMNTOJB-i5kJxu0Foph3dl08Wiju8t8uJqcdsCyj6OiHCfJYR9XT3kEEX0un2MaFPhQysO_rcFE2T1JnJu-t4qiI0cXEyEyAwpoMDtZJny2nUTclq5RzOQYhC_g9Iio-FFvBsErXIWBQbWGjWn5lr4K8A_PVEX8FIqLWIzuaNagbsZYC9PFu6-8lAWt5dMEPpBhA8pCNpjrmpOXZSCwWi-9HJxwv_DuByrO0-rtDwoC2jv8VNaoS8k3td45BaLpV-tkJcFhpQ_GNXWOyABdJbCiXz6ze4xw"
+        let token = "insert_me"
         
         let credentials = PSApiJWTCredentials()
         //credentials.token = try! decode(jwt: "")
@@ -29,8 +29,8 @@ final class PayseraParcelSDKTests: XCTestCase {
     }
     
     func testGetCourierAuthentication() {
-        let code = ""
-        let state = ""
+        let code = "insert_me"
+        let state = "insert_me"
         executeNonNil(
             parcelAPIClient.getCourierAuthentication(code: code, state: state),
             description: "CourierAuthentication URL must exist"
@@ -47,7 +47,7 @@ final class PayseraParcelSDKTests: XCTestCase {
     }
     
     func testGetTerminal() {
-        let id = "T4DDSnNzEk6WRGxaEzfQP0XzZBpboyVWz"
+        let id = "insert_me"
         
         executeNonNil(
             parcelAPIClient.getTerminal(id: id),
@@ -56,7 +56,7 @@ final class PayseraParcelSDKTests: XCTestCase {
     }
     
     func testGetTerminalSizes() {
-        let id = "T4DDSnNzEk6WRGxaEzfQP0XzZBpboyVWz"
+        let id = "insert_me"
         executeNonNil(
             parcelAPIClient.getTerminalSizesCount(id: id),
             description: "Terminal sizes must exist"
@@ -64,7 +64,7 @@ final class PayseraParcelSDKTests: XCTestCase {
     }
     
     func testGetParcel() {
-        let id = ""
+        let id = "insert_me"
         executeNonNil(
             parcelAPIClient.getParcel(id: id),
             description: "Parcel must exist"
@@ -72,7 +72,7 @@ final class PayseraParcelSDKTests: XCTestCase {
     }
     
     func testGetParcelStatusChanges() {
-        let id = ""
+        let id = "insert_me"
         executeNonNil(
             parcelAPIClient.getParcelStatusChanges(id: id),
             description: "Parcel statuses must exist"
@@ -88,7 +88,7 @@ final class PayseraParcelSDKTests: XCTestCase {
     
     func testGetPrice() {
         let filter = PSParcelPriceFilter()
-        filter.cellSize = "xs"
+        filter.cellSize = "insert_me"
         
         executeNonNil(
             parcelAPIClient.getPrice(payload: filter),
@@ -104,7 +104,7 @@ final class PayseraParcelSDKTests: XCTestCase {
     }
     
     func testGetCities() {
-        let countryCode = "LT"
+        let countryCode = "insert_me"
         executeNonNil(
             parcelAPIClient.getCities(countryCode: countryCode),
             description: "Cities must exist"
@@ -113,12 +113,12 @@ final class PayseraParcelSDKTests: XCTestCase {
     
     func testUpdateUser() {
         let request = PSUserUpdateRequest()
-        request.type = ""
-        request.phoneNumber = ""
-        request.email = ""
-        request.mainTerminalID = ""
-        request.oldPassword = ""
-        request.newPassword = ""
+        request.type = "insert_me"
+        request.phoneNumber = "insert_me"
+        request.email = "insert_me"
+        request.mainTerminalID = "insert_me"
+        request.oldPassword = "insert_me"
+        request.newPassword = "insert_me"
         
         executeNonNil(
             parcelAPIClient.updateUser(payload: request),
@@ -128,8 +128,8 @@ final class PayseraParcelSDKTests: XCTestCase {
     
     func testLogin() {
         let request = PSParcelLoginRequest()
-        request.username = ""
-        request.password = ""
+        request.username = "insert_me"
+        request.password = "insert_me"
         executeNonNil(
             parcelAPIClient.login(payload: request),
             description: "User must be logged in"
@@ -138,9 +138,9 @@ final class PayseraParcelSDKTests: XCTestCase {
     
     func testRegisterUser() {
         let request = PSParcelRegistrationRequest()
-        request.name = ""
-        request.password = ""
-        request.phoneNumber = ""
+        request.name = "insert_me"
+        request.password = "insert_me"
+        request.phoneNumber = "insert_me"
         executeNonNil(
             parcelAPIClient.registerUser(payload: request),
             description: "User must be registered"
@@ -156,8 +156,8 @@ final class PayseraParcelSDKTests: XCTestCase {
     }
     
     func testVerifyCode() {
-        let userID = ""
-        let code = ""
+        let userID = "insert_me"
+        let code = "insert_me"
         executeNonNil(
             parcelAPIClient.verifyCode(userID: userID, code: code),
             description: "Code must be verified"
@@ -165,7 +165,7 @@ final class PayseraParcelSDKTests: XCTestCase {
     }
     
     func testResendPhoneVerificationCode() {
-        let userID = ""
+        let userID = "insert_me"
         executeNonNil(
             parcelAPIClient.resendPhoneVerificationCode(userID: userID),
             description: "Code must be resent"
@@ -174,7 +174,7 @@ final class PayseraParcelSDKTests: XCTestCase {
     
     func testRegisterParcel() {
         let parcel = PSParcel()
-        let payOnReceive = true
+        let payOnReceive = true //insert_me
         executeNonNil(
             parcelAPIClient.registerParcel(payload: parcel, payOnReceive: payOnReceive),
             description: "Parcel must be registered"
@@ -183,7 +183,7 @@ final class PayseraParcelSDKTests: XCTestCase {
     
     func testUpdateParcel() {
         let parcel = PSParcel()
-        let payOnReceive = true
+        let payOnReceive = true //insert_me
         executeNonNil(
             parcelAPIClient.updateParcel(payload: parcel, payOnReceive: payOnReceive),
             description: "Parcel must be updated"
@@ -191,7 +191,7 @@ final class PayseraParcelSDKTests: XCTestCase {
     }
     
     func testUnlockParcel() {
-        let id = ""
+        let id = "insert_me"
         executeNonNil(
             parcelAPIClient.unlockParcel(id: id),
             description: "Parcel must be unlocked"
@@ -199,7 +199,7 @@ final class PayseraParcelSDKTests: XCTestCase {
     }
     
     func testReturnParcel() {
-        let id = ""
+        let id = "insert_me"
         executeNonNil(
             parcelAPIClient.returnParcel(id: id),
             description: "Parcel return must be requested"
