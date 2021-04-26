bbc4924d0b7e5ff6e0973b0b3c283ccb61c8a297
Gerald Adorza <gerald.adorza@paysera.net>
2021-04-26 10:42:10 +0800

    Cleanup

--- Package.swift
+++ Package.swift
@@ -13,8 +13,6 @@ let package = Package(
         .package(name: "PayseraCommonSDK", url: "https://github.com/paysera/swift-lib-common-sdk", from: "3.0.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PayseraParcelSDK",
             dependencies: ["PayseraCommonSDK"]),
