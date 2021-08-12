4bdb3d1ea5d547b8cfc33b89521ed36c9267b84e
Pattio <merfisku@gmail.com>
2021-08-12 13:53:48 +0300

    Update platforms

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "PayseraParcelSDK",
-    platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v9), .watchOS(.v2)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
         .library(name: "PayseraParcelSDK", targets: ["PayseraParcelSDK"]),
     ],
